84feccf894a25bb7bd56c9ddcfcb37c17d5a14bf
Mohammed Mahedi Hasan <mahediHasan7@gmail.com>
2025-09-03 01:15:11 +0800

    add validation annotations to ChangePasswordRequest, RegisterUserRequest, and UpdateUserRequest DTOs

--- src/main/java/com/mahedihasan/store/dtos/ChangePasswordRequest.java
+++ src/main/java/com/mahedihasan/store/dtos/ChangePasswordRequest.java
@@ -1,9 +1,16 @@
 package com.mahedihasan.store.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class ChangePasswordRequest {
+    @NotBlank(message = "Old Password is required")
+    @Size(min = 4, max = 12, message = "Please enter your old password between 4-12 characters")
     private String oldPassword;
+
+    @NotBlank(message = "New Password is required")
+    @Size(min = 4, max = 12, message = "Please enter a new password between 4-12 characters")
     private String newPassword;
 }
--- src/main/java/com/mahedihasan/store/dtos/UpdateUserRequest.java
+++ src/main/java/com/mahedihasan/store/dtos/UpdateUserRequest.java
@@ -1,9 +1,15 @@
 package com.mahedihasan.store.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UpdateUserRequest {
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @NotBlank(message = "Name is required")
+    @Email(message = "Valid email address is required")
     private String email;
 }
